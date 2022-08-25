5907a39e668c5cab2414894daf86c00f915fbaef
Bradley Mach <machdmach@yahoo.com>
2022-08-25 09:27:06 -0700

    Reorganized file and folder structure

--- NetApps/NetApps.Common/CommonLib/Database/CommonEntities.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace System.Data.Common
-{
-    public class LookupEntity
-    {
-        public int KeyId { get; set; }
-        public string Value { get; set; }
-        public string Desc { get; set; }
-    }
-}
--- NetApps/NetApps.WebAPI/wwwroot/js/site.js
+++
@@ -1,5 +0,0 @@
-﻿//https://localhost:44321/js/site.js
-
-// Write your JavaScript code here
-
-console.log('site.cs');
